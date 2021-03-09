8bcd7fcfac301e6ffdad4a4b591cbdda83fce7b9
mlicp1994 <mlicp1994@gmail.com>
2021-03-09 16:22:30 +0900

    Toby's Spring Study : Chapter 03 - calculator refactoring(BufferedReader -> LineReadReader)

--- tobyspring/src/main/java/springbook/learningtest/template/Calculator.java
+++ tobyspring/src/main/java/springbook/learningtest/template/Calculator.java
@@ -7,39 +7,47 @@ import java.io.IOException;
 public class Calculator {
 
 
-    public Integer calcSum(String filepath) throws IOException{
-
-        BufferedReaderCallback sumCallback =
-            new BufferedReaderCallback() {
+    public Integer calcSum(String filepath) throws IOException {
+        LineCallback sumCallback =
+            new LineCallback() {
                 @Override
-                public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                    Integer sum = 0;
-                    String line = null;
-
-                    while((line = br.readLine()) != null){
-                        sum += Integer.valueOf(line);
-                    }
-                    return sum;
+                public Integer doSomethingWithLine(String line, Integer value) {
+                    return value + Integer.valueOf(line);
                 }
         };
-        return fileReadTemplate(filepath,sumCallback);
+        return lineReadTemplate(filepath, sumCallback, 0);
     }
 
     public Integer calcMultiply(String filepath)throws IOException{
-
-        BufferedReaderCallback muliplyCallback =
-            new BufferedReaderCallback() {
+        LineCallback multiplyCallback =
+            new LineCallback() {
                 @Override
-                public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                    Integer multiply = 1;
-                    String line =null;
-                    while((line = br.readLine()) != null){
-                        multiply *= Integer.valueOf(line);
-                    }
-                    return multiply;
+                public Integer doSomethingWithLine(String line, Integer value) {
+                    return value * Integer.valueOf(line);
                 }
         };
-        return fileReadTemplate(filepath,muliplyCallback);
+        return lineReadTemplate(filepath, multiplyCallback, 1);
+    }
+
+    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException{
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+            Integer res = initVal;
+            String line = null;
+            while ((line = br.readLine()) != null){
+                res = callback.doSomethingWithLine(line, res);
+            }
+            return res;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw  e;
+        } finally {
+            if ( br != null){
+                try{ br.close(); }
+                catch (IOException e) { System.out.println(e.getMessage()); }
+            }
+        }
     }
 
     public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException{
